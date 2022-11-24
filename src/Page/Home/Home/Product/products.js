498fd8a33b64904f0f1fa16d36cc7e497aa37c42
Ashraf59 <ashrafams59b@gmail.com>
2022-11-24 21:58:58 +0600

    Home section created

---
+++ src/Page/Home/Home/Product/products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const products = () => {
+
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default products;
--- src/Page/Home/NewArrival/NewArrival.js
+++ src/Page/Home/NewArrival/NewArrival.js
@@ -4,15 +4,14 @@ import PrimaryButton from '../../../component/PrimaryButton';
 const NewArrival = () => {
     return (
         <div >
-            <h2 className="text-2xl font-semibold text-center mb-5 sm:text-4xl drop-shadow-lg p-6 rounded-lg">New Arrivals</h2>
+            <h2 className="text-4xl font-bold text-center mb-5 mt-20 sm:text-4xl drop-shadow-lg p-6 rounded-lg">New Arrivals</h2>
              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-20 justify-items-center'>
              <div className="card card-compact w-96 bg-base-100 shadow-xl">
              <figure><img src="https://www.mobiledokan.com/wp-content/uploads/2021/10/Apple-iPhone-13-Pro-image.jpg" alt="Shoes" /></figure>
                  <div className="card-body">
               <h2 className="card-title">Apple iPhone 13 Pro Max</h2>
               <p className='text-rose-500'>Price: BDT. 1,62,999.00</p>
-
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>iPhone 13 Pro Max exclusive new arrival phone</p>
              <div className="card-actions justify-end">
              <PrimaryButton>Buy Now</PrimaryButton>
              </div>
@@ -24,7 +23,7 @@ const NewArrival = () => {
               <h2 className="card-title"> Galaxy M53 5G</h2>
               <p className='text-rose-500'>Price: BDT. 52,999.00</p>
 
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <p>Galaxy M53 5G exclusive new arrival phone</p>
              <div className="card-actions justify-end">
              <PrimaryButton>Buy Now</PrimaryButton>
 
@@ -33,9 +32,9 @@ const NewArrival = () => {
             </div>
              <div className="card card-compact w-96 bg-base-100 shadow-xl">
              <figure><img src="https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Untitled4191.jpg" alt="Shoes" /></figure>
-                 <div className="card-body">
-              <h2 className="card-title">Xiaomi Mi Mix Fold</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
+                 <div className="card-body text-center ">
+              <h2 className="card-title text-center">Xiaomi Mi Mix Fold</h2>
+              <p>Xiaomi Mi Mix Fold exclusive new arrival phone</p>
               <p className='text-rose-500'>Price: BDT. 1,85,000.00</p>
              <div className="card-actions justify-end">
              <PrimaryButton>Buy Now</PrimaryButton>
